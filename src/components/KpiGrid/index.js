425ece839ed0d52e38e6132c3ea90629ce5b58e1
Wilmerson da Silva <wilmersondasilva@gmail.com>
2020-10-12 07:36:53 -0300

    Creates the component KpiGrid

---
+++ src/components/KpiGrid/index.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import KpiCard from 'components/KpiCard'
+
+const Wrapper = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    & > div + div {
+        margin-top: 30px;
+    }
+
+    @media (min-width: 650px) {
+        flex-direction: row;
+        justify-content: space-between;
+
+        & > div + div {
+            margin: 0 0 0 30px;
+        }
+    }
+`
+
+const defaultData = {
+    active_source: {},
+    weekly_active: {},
+    nps: {}
+}
+
+const KpisGrid = (props) => {
+    const { data } = props
+    const { active_source, weekly_active, nps } = Object.assign(
+        {},
+        defaultData,
+        data
+    )
+
+    return (
+        <Wrapper>
+            <KpiCard
+                title="Active sourcing"
+                currentValue={active_source.current_period}
+                lastValue={active_source.last_period}
+                backgroundColor="#ffa07a"
+            />
+            <KpiCard
+                title="Weekly active"
+                currentValue={weekly_active.current_period}
+                lastValue={weekly_active.last_period}
+                backgroundColor="#87cefa"
+            />
+            <KpiCard
+                title="NPS"
+                currentValue={nps.current_period}
+                lastValue={nps.last_period}
+                backgroundColor="#73dad6"
+            />
+        </Wrapper>
+    )
+}
+
+KpisGrid.propTypes = {
+    data: PropTypes.object
+}
+
+export default KpisGrid
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,24 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
+import KpiGrid from 'components/KpiGrid'
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+    padding-top: 50px;
+
+    @media (min-width: 650px) {
+        width: 80%;
+        max-width: 1150px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+`
 
 const Dashboard = () => {
-  return <Wrapper />;
-};
+    return (
+        <Wrapper>
+            <KpiGrid />
+        </Wrapper>
+    )
+}
 
-export default Dashboard;
+export default Dashboard
